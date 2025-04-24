62fa82a9414520a9a231524e5d6dd21a84e6da8d
Md Ridwan <111847409+Ridwan6172@users.noreply.github.com>
2025-04-24 14:44:10 +0600

    Update UpcomingMeetingsFilter.tsx

--- src/components/ProjectFilters/UpcomingMeetingsFilter.tsx
+++ src/components/ProjectFilters/UpcomingMeetingsFilter.tsx
@@ -8,11 +8,6 @@ interface UpcomingMeetingsFilterProps {
 
 export default function UpcomingMeetingsFilter({ upcomingMeetings, setUpcomingMeetings }: UpcomingMeetingsFilterProps) {
   return (
-    <div className="flex items-center mt-6">
-
-      <label htmlFor="upcomingMeetings" className="ml-2 text-sm font-medium">
-       
-      </label>
-    </div>
+   
   );
 }
